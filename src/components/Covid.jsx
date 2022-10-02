06500e06064596e698048cbff4aa4d6c2d76eace
anish0803 <105308559+anish0803@users.noreply.github.com>
2022-10-02 23:22:56 +0530

    css update

--- src/components/Covid.jsx
+++ src/components/Covid.jsx
@@ -1,48 +1,42 @@
+import React, { useState, useEffect } from "react";
+import "./Covid.css";
 
-
-
-
-import  React, { useState, useEffect} from 'react'
-import './Covid.css'
-
-export function Covid(){
-
+export function Covid() {
   const [data, setData] = useState([]);
-    const [theme, setTheme] = useState('dark-mode');
+  const [theme, setTheme] = useState("dark-mode");
 
-    useEffect(() => {
-        document.body.className = theme;
-    }, [theme]);
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
 
-    const toggle = () => {
-        if (theme === "dark-mode") {
-            setTheme('light-mode');
-        } else {
-            setTheme('dark-mode');
-        }
+  const toggle = () => {
+    if (theme === "dark-mode") {
+      setTheme("light-mode");
+    } else {
+      setTheme("dark-mode");
     }
+  };
 
-    const getCovidData = async () => {
-            const res = await fetch('https://data.covid19india.org/data.json');
-            const actualData = await res.json();
-            console.log(actualData.statewise);
-            setData(actualData.statewise)
-                  } 
+  const getCovidData = async () => {
+    const res = await fetch("https://data.covid19india.org/data.json");
+    const actualData = await res.json();
+    console.log(actualData.statewise);
+    setData(actualData.statewise);
+  };
 
-    
+  useEffect(() => {
+    getCovidData();
+  }, []);
 
-    useEffect(() => {
-        getCovidData();
-    }, []);
-
-
-  return(
+  return (
     <>
-      <div className='container'>
-        <div className='heading'><h1>Covid-19 LIVE UPDATES</h1></div>
-        <table className='table'>
+      <div className="container">
+        <div className="heading">
+          <h1>Covid-19 LIVE UPDATES</h1>
+        </div>
+        <table className="table">
           <thead>
-            <tr>
+            <tr headinfo>
               <th>STATE</th>
               <th>STATE CODE</th>
 
@@ -53,6 +47,7 @@ export function Covid(){
               <th>DELTA DEATH</th>
 
               <th>ACTIVE</th>
+
               <th>RECOVERED</th>
               <th>DELTA RECOVERED</th>
 
@@ -60,9 +55,8 @@ export function Covid(){
             </tr>
           </thead>
           <tbody>
-
             {data.map((currentElement, index) => {
-              return(
+              return (
                 <tr>
                   <td>{currentElement.state}</td>
                   <td>{currentElement.statecode}</td>
@@ -79,16 +73,17 @@ export function Covid(){
 
                   <td>{currentElement.lastupdatedtime}</td>
                 </tr>
-              )
+              );
             })}
-            
           </tbody>
         </table>
 
-        <div className='changeTheme'>
-                    <button class="btn" onClick={() => toggle()}>Toggle Mode</button>
-                </div>
+        <div className="changeTheme">
+          <button class="btn" onClick={() => toggle()}>
+            Toggle Mode
+          </button>
+        </div>
       </div>
     </>
-  )
+  );
 }
